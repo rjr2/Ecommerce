e1a03566c30dacde5b7ce18086f030a64508af5f
RJR <80534043+rjr2@users.noreply.github.com>
2021-11-03 14:43:32 -0400

    sequelize failing to connect to localhost

--- models/index.js
+++ models/index.js
@@ -5,22 +5,21 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.hasOne(Category, {
+Product.belongsTo(Category, {
   foreignKey: 'category_id',
 });
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'product_key',
-  onDelete: 'cascade'
+  foreignKey: {
+    name: 'category_id'
+  }
 });
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, {
-  foreignKey: 'productTag_id'
-})
+Product.belongsToMany(Tag, { through: ProductTag });
+  
+
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, {
-  foreignKey: 'producttag'
-})
+Tag.belongsToMany(Product, { through: ProductTag });
 
 
 module.exports = {
